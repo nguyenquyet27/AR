24f6d4f06d60eee5566f64c164997b1b3b915743
Kihiru <ndquyet27@gmail.com>
2019-12-21 18:11:34 +0700

    clean code test.py

--- main.py
+++ main.py
@@ -4,37 +4,47 @@ import process_func as pf
 from objloader_simple import *
 import math
 
-MIN_MATCHES = 220
+from pygame.locals import *
+from pygame.constants import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+MIN_MATCHES = 100
 def main():
     """
     This functions loads the target surface image,
     """
     homography = None 
-    camera_parameters = np.array([[800, 0, 320], [0, 800, 240], [0, 0, 1]])
+    camera_parameters = np.array([[600, 0, 320], [0, 600, 240], [0, 0, 1]])
     orb = cv2.ORB_create()
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
     model = cv2.imread('template/joker.jpg')
-    obj = OBJ('models/11571_Gingerbread_cookie_male_V2_l2.obj',swapyz=True)
-    img1 = pf.image_proc(model, 0.5)
+    obj = OBJ('models/fox.obj',swapyz=True)
+    img1 = pf.image_proc(model, 1)
     kp_model, des_model = orb.detectAndCompute(img1, None)
     cap = cv2.VideoCapture(0)
 
     while True:
         ret, frame = cap.read()
-        img2 = pf.image_proc(frame, 0.5)
+        img2 = pf.image_proc(frame, 1)
         # print(frame.shape)
         if not ret:
             print ("Unable to capture video")
             return 
         kp_frame, des_frame = orb.detectAndCompute(img2, None)
+        if (des_frame is None): 
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            continue
         matches = bf.match(des_model, des_frame)
         matches = sorted(matches, key=lambda x: x.distance)
 
         if (len(matches) > MIN_MATCHES):
             src_pts = np.float32([kp_model[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
             dst_pts = np.float32([kp_frame[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
-            homography, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            homography = pf.computeHomography(src_pts ,dst_pts)
             h, w = img1.shape
             pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
             dst = cv2.perspectiveTransform(pts, homography)
@@ -44,7 +54,7 @@ def main():
                     # obtain 3D projection matrix from homography matrix and camera parameters
                     projection = pf.projection_matrix(camera_parameters, homography)
                     # project cube or model
-                    frame = pf.render(frame, obj, projection, img1, True)
+                    frame = pf.render(frame, obj, projection, img1, False)
                 except:
                     pass
             
