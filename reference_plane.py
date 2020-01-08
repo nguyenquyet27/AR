b05be03a640aeb7768cc37bbc46bc0905c83d5f5
ry <conlocxoaythuytrieu@gmail.com>
2020-01-08 18:09:12 +0700

    push to change filename in template

--- reference_plane.py
+++ reference_plane.py
@@ -10,10 +10,10 @@ class ReferencePlane(object):
     def __init__(self, plane_path):
         print('[INFO]Make reference surface ...')
         self.orb = cv2.ORB_create()
-
-        # TODO: using more planar
+        
         self.ref_plane = cv2.imread(os.path.join(os.getcwd(), plane_path))
-        # Set size of image in dataset if you want. Change here (230,300)
+        
+        # Set size of image in ./template/ if you want. Change here (230,300)
         self.image_ref = image_proc(cv2.resize(self.ref_plane, (230,300)), 1)
         self.height, self.width = self.image_ref.shape
 
